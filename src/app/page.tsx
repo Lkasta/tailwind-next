e05b3ca811f90ac4b5b9a579ee1c74812043401e
Luiz Karpinski <lkastabackup@gmail.com>
2023-09-06 21:06:43 -0300

    Finalizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,220 @@
+import { Button } from '@/components/Button'
+import * as FileInput from '@/components/Form/FileInput'
+import { Select } from '@/components/Form/Select/Index'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
+import { Textarea } from '@/components/Form/Textarea'
+import * as Input from '@/components/Input'
+import { Settings } from '@/components/Settings'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
+
 export default function Home() {
   return (
     <>
-      <h1>Teste</h1>
+      <h1 className="text-3xl font-medium text-zinc-900 dark:text-zinc-100">
+        Settings
+      </h1>
+
+      <Settings />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex flex-col justify-between gap-4 border-b border-zinc-200 pb-5 dark:border-zinc-700 lg:flex-row lg:items-center">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900 dark:text-zinc-100">
+              Personal Info
+            </h2>
+            <span className="text-sm text-zinc-500 dark:text-zinc-400">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="items center flex gap-2">
+            <Button variant="outline" type="button">
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
+              Save
+            </Button>
+          </div>
+        </div>
+        <form
+          id="settings"
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200 dark:divide-zinc-700"
+        >
+          <div className="flex flex-col gap-3 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="firstName"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Name
+            </label>
+            <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2">
+              <Input.InputRoot>
+                <Input.InputControl id="firstName" defaultValue="Luiz" />
+              </Input.InputRoot>
+
+              <div className="flex flex-col gap-3 lg:block">
+                <label
+                  htmlFor="lastName"
+                  className="text-sm dark:text-zinc-300 dark:text-zinc-400 lg:sr-only"
+                >
+                  Last Name
+                </label>
+
+                <Input.InputRoot>
+                  <Input.InputControl id="lastName" defaultValue="Karpinski" />
+                </Input.InputRoot>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="email"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Email address
+            </label>
+            <Input.InputRoot>
+              <Input.InputPrefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </Input.InputPrefix>
+              <Input.InputControl
+                id="email"
+                type="email"
+                defaultValue="lkasta@lkasta.com.br"
+              />
+            </Input.InputRoot>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="photo"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Your photo
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                This will be displayed on your profile.
+              </span>
+            </label>
+            <FileInput.Root className="flex flex-col gap-5 lg:flex-row lg:items-start">
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
+
+            <div />
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="role"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Role
+            </label>
+            <Input.InputRoot>
+              <Input.InputControl id="role" defaultValue="Dev" />
+            </Input.InputRoot>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="country"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Country
+            </label>
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="timezone"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Timezone
+            </label>
+            <Select placeholder="Select a timezone...">
+              <SelectItem value="br" text="Pacific Standard Time (UTC-08:00)" />
+              <SelectItem value="us" text="America São Paulo (UTC-03:00)" />
+            </Select>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="bio"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Bio
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Write a short introduction.
+              </span>
+            </label>
+            <div className="space-y-3">
+              <div className="flex flex-col gap-3 lg:grid lg:grid-cols-2">
+                <Select placeholder="" defaultValue="normal">
+                  <SelectItem value="normal" text="Normal Text" />
+                  <SelectItem value="md" text="Markdown" />
+                </Select>
+
+                <div className="flex items-center gap-1">
+                  <Button type="button" variant="ghost">
+                    <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button type="button" variant="ghost">
+                    <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button type="button" variant="ghost">
+                    <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button type="button" variant="ghost">
+                    <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button type="button" variant="ghost">
+                    <ListOrdered
+                      className="h-4 w-4 text-zinc-500"
+                      strokeWidth={3}
+                    />
+                  </Button>
+                </div>
+              </div>
+              <Textarea
+                id="bio"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-3 pt-5 lg:grid lg:grid-cols-form">
+            <label
+              htmlFor="projects"
+              className="text-sm dark:text-zinc-300 dark:text-zinc-400"
+            >
+              Portfolio projects
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <FileInput.Root>
+              <FileInput.Trigger />
+              <FileInput.FileList />
+              <FileInput.Control multiple />
+            </FileInput.Root>
+            <div />
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <Button variant="outline" type="button">
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
+              Save
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { Logo } from './Logo'
+
+import * as Collapssible from '@radix-ui/react-collapsible'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Menu,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { Button } from '../Button'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
+import { NavItem } from './NavItem'
+import { Profile } from './Profile'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+
+export function Sidebar() {
+  return (
+    <Collapssible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 dark:border-zinc-800  dark:bg-zinc-900 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between data-[state=closed]:h-screen">
+        <Logo />
+        <Collapssible.Trigger className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="w6 h-6" />
+          </Button>
+        </Collapssible.Trigger>
+      </div>
+
+      <Collapssible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <InputRoot>
+          <InputPrefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
+
+        <nav className="space-y-0.5">
+          <NavItem title="Home" icon={Home} />
+          <NavItem title="Dashboard" icon={BarChart} />
+          <NavItem title="Projects" icon={SquareStack} />
+          <NavItem title="Tasks" icon={CheckSquare} />
+          <NavItem title="Reporting" icon={Flag} />
+          <NavItem title="Users" icon={Users} />
+        </nav>
+
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
+
+          <UsedSpaceWidget />
+
+          <div className="h-px bg-zinc-200 dark:bg-zinc-700" />
+
+          <Profile />
+        </div>
+      </Collapssible.Content>
+    </Collapssible.Root>
+  )
+}
